30498508c601bf6a72da09d4aa85351963a1e7e6
ludidilu <ludidilu@163.com>
2018-04-24 13:38:43 +0800

    a

---
+++ PackageTypeTag.cs
@@ -0,0 +1,15 @@
+﻿namespace FinalWar_proto
+{
+    public enum CsPackageTag
+    {
+        BattleData,
+        Login,
+        BattleManagerAction,
+    }
+
+    public enum ScPackageTag
+    {
+        BattleData,
+        PlayerState,
+    }
+}
